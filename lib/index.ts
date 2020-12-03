261b0461e44c3cc3578406b575ce91c1fdb196b3
Shunyu Wang <w_shunyu@live.concordia.ca>
2020-12-02 22:05:09 -0500

    init project

---
+++ lib/index.ts
@@ -0,0 +1,8 @@
+/**
+* @Method: Returns the plural form of any noun.
+* @Param {string}
+* @Return {string}
+*/
+export function hello (str: string = "abcd") : void {
+    console.log(hello + " str");
+  }
